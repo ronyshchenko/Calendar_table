07e85c74b2e3e46262d3d41e59f2f7659ffea1a9
ronyshchenko <roma.onyshchenko@gmail.com>
2019-05-23 16:32:23 +0300

    next_commit

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
 'use strict';
-'use strict';
+
 function createCalendar(id, year, month) {
-  var elem = document.getElementById(id);
-    let mon = month - 1;     
-    let d = new Date(year, mon);
+    let elem = document.getElementById(id);
+    const monthFromZero = month - 1;     
+    let dateForTable = new Date(year, monthFromZero);
     let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
-    for (var i = 0; i < getDay(d); i++) {
+    
+    for (let i = 0; i < getDay(dateForTable); i++) {
       table += '<td></td>';
     }
-    while (d.getMonth() == mon) {
-      table += '<td>' + d.getDate() + '</td>';
-      if (getDay(d) % 7 == 6) { 
+   
+    while (dateForTable.getMonth() == monthFromZero) {
+      table += `<td> ${dateForTable.getDate()} </td>`;
+      
+      if (getDay(dateForTable) % 7 == 6) { 
         table += '</tr><tr>';
       }
-      d.setDate(d.getDate() + 1);
+      dateForTable.setDate(dateForTable.getDate() + 1);
     }
-    if (getDay(d) != 0) {
-      for (var i = getDay(d); i < 7; i++) {
+    
+    if (getDay(dateForTable) !== 0) {
+      for (let i = getDay(dateForTable); i < 7; i++) {
         table += '<td></td>';
       }
     }
     table += '</tr></table>';
     elem.innerHTML = table;
   }
+  
   function getDay(date) {
-    var day = date.getDay();
+    let day = date.getDay();
     if (day == 0) day = 7;
     return day - 1;
   }
+  
   createCalendar("calendar", 2019, 5)
